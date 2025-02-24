f8acacc1831800ab530a3576eb9ce87cf60d8f4e
Elise Albrecht <118198011+Vitally05@users.noreply.github.com>
2025-02-24 09:57:37 -0500

    Simplified functions

--- Categorizer_test.py
+++ Categorizer_test.py
@@ -7,7 +7,7 @@ import json
 import re
 
 class CategorizerImages:
-    def __init__(self, client, directory="test_data", allowed_extensions=None):
+    def __init__(self, client, directory="test_data", allowed_extensions=None, model="gpt-4o-mini"):
         """
         :param client: Instance du client OpenAI.
         :param directory: Répertoire où se trouvent les images.
@@ -18,6 +18,7 @@ class CategorizerImages:
         self.client = client
         self.directory = directory
         self.allowed_extensions = allowed_extensions
+        self.model = model
 
     def encode_image(self, image_path):
         try:
@@ -31,36 +32,44 @@ class CategorizerImages:
         image_paths = [os.path.join(self.directory, filename) for filename in os.listdir(self.directory) if os.path.splitext(filename)[1].lower() in self.allowed_extensions]
         return image_paths
 
-    def chat(self, base64_images):
+    def send_gpt_request(self, message, extracting_json=False):
         try:
-            # Liste pour chaque image et chaque texte associé
-            content_list = []
-            for i, base64_image in enumerate(base64_images):
-                content_list.append({
-                    "type": "text",
-                    "text": f"Décris moi l'image {i+1} avec 5 mots-clés. Le format est le suivant : [mot-clé1, mot-clé2, mot-clé3, mot-clé4, mot-clé5]"
-                })
-                # Ajoute l'image encodée
-                content_list.append({
-                    "type": "image_url",
-                    "image_url": {"url": f"data:image/jpeg;base64,{base64_image}"}
-                })
-
-            messages = [
-                {
-                    "role": "user",
-                    "content": content_list
-                }
-            ]
-
-            chat_completion = self.client.chat.completions.create(
-                model="gpt-4o-mini", messages=messages
+            response = self.client.chat.completions.create(
+                model=self.model, messages=message
             )
-            return chat_completion.choices[0].message.content.strip()
+
+            result = response.choices[0].message.content.strip()
+            if extracting_json:
+                result = self.extract_json(result)
+
+            return result
 
         except Exception as e:
-            print(f"Erreur OpenAI lors de l'extraction des mots-clés : {e}")
-            return None
+            print(f"Erreur OpenAI : {e}")
+
+    def gpt_get_key_words(self, base64_images):
+
+        # Liste pour chaque image et chaque texte associé
+        content_list = []
+        for i, base64_image in enumerate(base64_images):
+            content_list.append({
+                "type": "text",
+                "text": f"Décris moi toutes les images avec 5 mots-clés. Le format est le suivant Image i : [mot-clé1, mot-clé2, mot-clé3, mot-clé4, mot-clé5]"
+            })
+            content_list.append({
+                "type": "image_url",
+                "image_url": {"url": f"data:image/jpeg;base64,{base64_image}"}
+            })
+
+        messages = [
+            {
+                "role": "user",
+                "content": content_list
+            }
+        ]
+
+        return self.send_gpt_request(messages, extracting_json=False)
+
 
     def extract_json(self, response_text):
         """
@@ -82,43 +91,42 @@ class CategorizerImages:
             print("Aucun JSON trouvé dans la réponse.")
             return None
 
-    def categorize_photos(self, keywords_output, image_paths):
-        try:
-            prompt = f"""Voici les listes de mots-clés obtenues pour chaque image (dans l'ordre) : {keywords_output}
+    def gpt_get_categories(self, keywords_output, image_paths):
+        """
+        Utilise les mots-clés extraits pour regrouper les images similaires en catégories.
+        Les images sont identifiées par leur ordre dans la liste.
+        """
 
-            L'ordre des images est le suivant : {chr(10).join([f"Image {i+1} : {path}" for i, path in enumerate(image_paths)])}
+        # Préparation d'un prompt détaillé incluant le résultat des mots-clés et l'ordre des images
+        prompt = f"""Voici les listes de mots-clés obtenues pour chaque image (dans l'ordre) : {keywords_output}
 
-            En te basant sur ces informations, regroupe les images similaires dans des catégories.
-            Retourne uniquement le résultat au format JSON en indiquant pour chaque catégorie la liste des indices des images (par exemple : "Image 1", "Image 3", etc.) qui appartiennent à cette catégorie.
-            Exemple de format :
-            {{
-                "categorie1": [ "Image 1", "Image 3" ],
-                "categorie2": [ "Image 2", "Image 4" ]
-            }}
-            """
-            messages = [
-                {
-                    "role": "user",
-                    "content": prompt
-                }
-            ]
-            chat_completion = self.client.chat.completions.create(
-                model="gpt-4o-mini", messages=messages
-            )
-            response_text = chat_completion.choices[0].message.content.strip()
-            return self.extract_json(response_text)
-        
-        except Exception as e:
-            print(f"Erreur OpenAI lors de la catégorisation : {e}")
-            return None
+        L'ordre des images est le suivant : {chr(10).join([f"Image {i} : {path}" for i, path in enumerate(image_paths)])}
+
+        En te basant sur ces informations, regroupe les images similaires dans des catégories. Une catégorie est décrite par un seul mot-clé.
+        Retourne le résultat au format JSON en indiquant pour chaque catégorie la liste des indices des images (par exemple : "Image 1", "Image 3", etc.) qui appartiennent à cette catégorie.
+        Le format attendu est :
+        {{
+            "categorie1": [ "Image 1", "Image 3" ],
+            "categorie2": [ "Image 2", "Image 4" ],
+            ...
+        }}
+        """
+        messages = [
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ]
+
+        return self.send_gpt_request(messages, extracting_json=True)
 
     def process(self):
         image_paths = self.get_image_paths()
         base64_images = [self.encode_image(path) for path in image_paths]
-        keywords_output = self.chat(base64_images)
+        keywords_output = self.gpt_get_key_words(base64_images)
         print("Mots-clés par image :")
         print(keywords_output)
-        categories = self.categorize_photos(keywords_output, image_paths)
+        categories = self.gpt_get_categories(keywords_output, image_paths)
         print("Catégorisation des images :")
         print(categories)
 
