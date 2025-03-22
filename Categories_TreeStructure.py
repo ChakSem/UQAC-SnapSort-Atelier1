9b54d0183416a1c3e67c4a19ceef9fd5f24a0419
Salim mansouri <salim@Mac.local>
2025-03-22 16:02:56 -0400

    Petit ajustement

--- Categories_TreeStructure.py
+++ Categories_TreeStructure.py
@@ -2,7 +2,7 @@ import os
 import shutil
 import pandas as pd
 
-def create_category_folders_from_csv(csv_file, source_directory, destination_directory):
+def create_category_folders_from_csv(csv_file, destination_directory):
     df = pd.read_csv(csv_file)
 
     if 'categories' not in df.columns:
@@ -17,18 +17,15 @@ def create_category_folders_from_csv(csv_file, source_directory, destination_dir
         category_folder = os.path.join(destination_directory, category)
         os.makedirs(category_folder, exist_ok=True)
 
-        images_in_category = df[df['categories'] == category]['image_name'].tolist()
+        images_in_category = df[df['categories'] == category]['path'].tolist()
 
-        for image_name in images_in_category:
-            source_path = os.path.join(source_directory, image_name)
-            destination_path = os.path.join(category_folder, image_name)
-
+        for source_path in images_in_category:
             if os.path.exists(source_path):
+                destination_path = os.path.join(category_folder, os.path.basename(source_path))
                 shutil.copy(source_path, destination_path)
                 print(f"Copié : {source_path} -> {destination_path}")
             else:
                 print(f"Fichier non trouvé : {source_path}")
 
 if __name__ == "__main__":
-    create_category_folders_from_csv(csv_file="test_data_copy.csv", source_directory="test_data_copy", 
-                                    destination_directory="TreeStructure")
+    create_category_folders_from_csv(csv_file="test_data_copy.csv", destination_directory="TreeStructure")
